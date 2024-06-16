2d6f0b3751e07539417e044029bee16dbc067efa
Roman Khabarov <habermanrom@gmail.com>
2024-06-16 20:41:40 +1000

    second commit

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class MovieManagerTest {
+import org.example.MovieManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MovieManagerTest {
+
+
+    @Test
+    public void noAddFilm() {
+        MovieManager manager = new MovieManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addOneFilm() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addTwoFilm() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+
+        String[] expected = {"Film 1", "Film 2"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findAllFilm() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] expected = {"Film 1", "Film 2 ", "Film 3"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastFilm() {
+        MovieManager manager = new MovieManager();
+
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
